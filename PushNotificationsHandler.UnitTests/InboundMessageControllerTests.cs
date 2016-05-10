e048d12fed41c28de82cfda24f80d837a439f3f9
trowsell86 <tom.rowsell@esendex.com>
2016-05-10 16:51:50 +0100

    Added unit tests for push handler demo

---
+++ PushNotificationsHandler.UnitTests/InboundMessageControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Web.Http;
+using Moq;
+using NUnit.Framework;
+using PushNotificationsHandler.Controllers.Api;
+using PushNotificationsHandler.Models;
+using PushNotificationsHandler.Models.Api;
+
+namespace PushNotificationsHandler.UnitTests
+{
+    [TestFixture]
+    public class InboundMessageControllerTests
+    {
+        [Test]
+        public void GivenAnInboundMessage_WhenMessageIsPassedToTheApi_ThenInboundProcessorReceivesMessage()
+        {
+            var inboundProcessor = new Mock<IInboundProcessor>();
+            var controller = new InboundMessagesController(inboundProcessor.Object);
+
+
+            var inboundMessage = new InboundMessage
+                                 {
+                                     AccountId = Guid.NewGuid(),
+                                     From = "07752192872",
+                                     Id = Guid.NewGuid(),
+                                     MessageText = "Hello Bob",
+                                     MessageId = Guid.NewGuid(),
+                                     Now = DateTime.Now,
+                                     To = "0778723133"
+                                 };
+            controller.Post(inboundMessage);
+
+            inboundProcessor.Verify(ip => ip.Push(inboundMessage));
+        }
+    }
+}
---
+++ PushNotificationsHandler/Controllers/Api/InboundMessagesController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http;
+using PushNotificationsHandler.Models;
+using PushNotificationsHandler.Models.Api;
+
+namespace PushNotificationsHandler.Controllers.Api
+{
+    public class InboundMessagesController : ApiController
+    {
+        private readonly IInboundProcessor _inboundProcessor;
+
+        public InboundMessagesController(IInboundProcessor inboundProcessor)
+        {
+            _inboundProcessor = inboundProcessor;
+        }
+
+        public void Post(InboundMessage message)
+        {
+            _inboundProcessor.Push(message);
+        }
+    }
+}
