b61db91001d187593654829b081ae2698b0a5e22
trowsell86 <tom.rowsell@esendex.com>
2016-05-10 14:59:13 +0100

    PushNotificationsDemo - Sample animate your text app to demonstrate use of Esendex push notifications api

---
+++ PushNotificationsHandler/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PushNotificationsHandler.MvcApplication" Language="C#" %>
---
+++ PushNotificationsHandler/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PushNotificationsHandler.Startup))]
+namespace PushNotificationsHandler
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            
+        }
+    }
+}
