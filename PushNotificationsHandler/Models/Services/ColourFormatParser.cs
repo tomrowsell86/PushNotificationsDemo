09fcbc61f15bb6d8a0f0870c8c1f3dfed63ce351
trowsell86 <tom.rowsell@esendex.com>
2016-05-12 13:32:03 +0100

    Added colour parser tests

--- PushNotificationsHandler/Models/Services/ColourFormatParser.cs
+++ PushNotificationsHandler/Models/Services/ColourFormatParser.cs
@@ -1,14 +1,20 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace PushNotificationsHandler.Models.Services
 {
     public class ColourFormatParser : IColourFormatParser
     {
+        private const string MessagePartColourPattern = @"\{colour:(?<colour>#[0-9A-Fa-f]{6})\}(?<messagePart>.+)\{colour\}";
+
         public IList<ColourFormattedPart> GetFormattedParts(string messageText)
         {
-            throw new NotImplementedException();
-            //Regex.Matches(messageText, @"\{colour:#[0-9A-Fa-f]{6}\}(<?messagePart>.+){colour}").Cast<Match>().Select(m => new ColourFormattedPart(m.Value, m.Groups["messagePart"]))
+            return Regex.Matches(messageText, MessagePartColourPattern)
+                .Cast<Match>()
+                .Select(m => new ColourFormattedPart(m.Groups["colour"].Value, m.Groups["messagePart"].Value))
+                .ToList();
         }
     }
 }
